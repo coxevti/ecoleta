03a250313ed863c10fdd00a44944ccdda0e1a6f2
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2020-06-11 20:38:45 -0400

    Web -  Added style global

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import StyleGlobal from 'styles/global';
 
-const App: React.FC = () => <h1>Ecoleta</h1>;
+const App: React.FC = () => (
+  <>
+    <h1>Ecoleta</h1>
+    <StyleGlobal />
+  </>
+);
 
 export default App;
