ac1524c1afc7e5cca29e89da0919b51ae7c9a0d0
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2020-06-14 08:54:14 -0400

    Api - Added initial routes

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import 'reflect-metadata';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req,res) => {
-  return res.json({message: 'Ecoleta'});
-});
+app.use(routes);
 
 const port = process.env.SERVER_PORT || 3339;
+
 app.listen(port, () => {
-  console.log(`Server initialized, running in ${process.env.NODE_ENV} mode on port ${port}`);
+  console.log(
+    `🚀️ Server initialized, running in ${process.env.NODE_ENV} mode on port ${port}`,
+  );
 });
