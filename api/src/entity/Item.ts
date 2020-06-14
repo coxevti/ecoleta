4faa15f6387124a6aa5778857d4eef8dc450db2e
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2020-06-14 08:07:04 -0400

    Api - Added entity item

---
+++ api/src/entity/Item.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('points')
+class Item {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
+
+export default Item;
